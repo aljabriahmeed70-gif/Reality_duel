410b78dd3e3b07ac752284dbc2113dab1ba41ed8
aljabriahmeed70-gif <aljabriahmeed70@gmail.com>
2026-08-12 01:51:32 +0300

    Create Talent class with properties and constructor

---
+++ lib/models/talent.dart
@@ -0,0 +1,15 @@
+class Talent {
+  final String id;
+  final String name;
+  final String skill;
+  final String bio;
+  final String location;
+
+  const Talent({
+    required this.id,
+    required this.name,
+    required this.skill,
+    required this.bio,
+    required this.location,
+  });
+}
