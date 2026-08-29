319b11257164e952935b84a22afbe7cb625fa198
aljabriahmeed70-gif <aljabriahmeed70@gmail.com>
2026-08-30 01:38:08 +0300

    Add file_picker package import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:file_picker/file_picker.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
